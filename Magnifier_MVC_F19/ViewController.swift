512eba6d89d4f206cee320bbec0caa8a452d7bdc
NicholasRosato <rosaton@purdue.edu>
2019-09-06 11:50:48 -0400

    Initial Commit

---
+++ Magnifier_MVC_F19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magnifier_MVC_F19
+//
+//  Created by Nicholas Rosato on 9/6/19.
+//  Copyright © 2019 Rosato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
