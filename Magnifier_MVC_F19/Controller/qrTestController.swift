8c502ef9d69df91b1f442adc6bf988c9a1ee2756
NicholasRosato <rosaton@purdue.edu>
2019-09-08 19:07:18 -0400

    Add qrTestController.swift file

---
+++ Magnifier_MVC_F19/Controller/qrTestController.swift
@@ -0,0 +1,17 @@
+//
+//  qrTestController.swift
+//  Magnifier_MVC_F19
+//
+//  Created by Nicholas Rosato on 9/8/19.
+//  Copyright © 2019 Rosato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class qrTestController : UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
